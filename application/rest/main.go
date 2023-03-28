deb7699ef3adfa5f069d25e0d8eef988f90c8796
Farhandika <18220015@std.stei.itb.ac.id>
2023-03-29 01:12:33 +0700

    impr

--- application/rest/main.go
+++ application/rest/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"project-template/internal/config"
 )
 
 func main() {
-	conf, err := config.Init()
+	conf, err := configLoader()
 	if err != nil {
 		log.Fatal(err)
-		log.Println()
 	}
 	if err = application(conf); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func configLoader() (*config.MainConfig, error) {
+	conf, err := config.Init()
+	if err != nil {
+		return nil, errors.New("Cannot find secrets.config.json")
+	}
+	return conf, nil
 }
